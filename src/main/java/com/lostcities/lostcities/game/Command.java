5b29ee9736a171ef2ae2320cf326655baf9db786
Nathan Johnson <nathancj224@gmail.com>
2018-08-20 21:27:09 -0400

    Added Command game object

---
+++ src/main/java/com/lostcities/lostcities/game/Command.java
@@ -0,0 +1,10 @@
+package com.lostcities.lostcities.game;
+
+public class Command {
+
+    private Player player;
+    private Card playCard;
+    private Card discardCard;
+    private Color drawCardColor;
+
+}
