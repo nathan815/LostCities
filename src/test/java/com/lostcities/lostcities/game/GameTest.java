1b57d86c952851020f4a657ccff5cdac72e7938c
Nathaniel Johnson <nathancj224@gmail.com>
2018-08-22 01:58:25 -0400

    Add travis CI config

--- src/test/java/com/lostcities/lostcities/game/GameTest.java
+++ src/test/java/com/lostcities/lostcities/game/GameTest.java
@@ -42,13 +42,13 @@ public class GameTest {
         assertEquals("Player 2 has 8 cards", 8, player2.getHand().size());
 
         assertEquals(
-                "Verify order of Player 1's hand.",
+                "Player 1's hand is valid",
                 "[BLUE_9_0, YELLOW_4_0, RED_5_0, YELLOW_3_0, YELLOW_9_0, BLUE_1_1, WHITE_2_0, RED_4_0]",
                 player1.getHand().toString()
                 );
 
         assertEquals(
-                "Verify order of Player 2's hand.",
+                "Player 2's hand is valid",
                 "[BLUE_6_0, WHITE_1_0, WHITE_3_0, BLUE_2_0, GREEN_5_0, BLUE_3_0, WHITE_6_0, BLUE_8_0]",
                 player2.getHand().toString());
     }
