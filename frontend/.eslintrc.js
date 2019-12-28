a8ac2bd411aecd3b9e209fa9186b4ae1df3f401d
Nathan Johnson <nathancj224@gmail.com>
2019-12-28 03:00:21 -0500

    Configure prettier to prefer single quotes

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "prettier/prettier": ["error"]
   },
   parserOptions: {
     parser: "babel-eslint"
