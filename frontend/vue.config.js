89d63d454f368cd000272194494de800851b6b24
Nathan Johnson <nathancj224@gmail.com>
2019-12-28 02:31:51 -0500

    Change dev ports for frontend and backend

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,13 +1,17 @@
 const path = require('path');
 
+const FRONTEND_PORT = 8088;
+const BACKEND_PORT = 8089;
+const BACKEND_URL = `http://localhost:${BACKEND_PORT}/`;
+
 module.exports = {
     lintOnSave: false,
     devServer: {
-        port: 8088,
+        port: FRONTEND_PORT,
         proxy: {
             // proxy all requests starting with /api to backend api
             '/api': {
-                target: 'http://localhost:8080/',
+                target: BACKEND_URL,
                 changeOrigin: true
             }
         }
