c0618522a560fe86485e3c419bca7a52bad6bd06
Nathan Johnson <nathancj224@gmail.com>
2020-01-02 03:07:39 -0500

    Add ColorTest

---
+++ backend/src/test/java/com/lostcities/lostcities/domain/model/game/ColorTest.java
@@ -0,0 +1,23 @@
+package com.lostcities.lostcities.domain.model.game;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ColorTest {
+    @Test
+    public void fromString_allCaps_shouldReturnCorrectInstance() {
+        assertEquals(Color.BLUE, Color.fromString("BLUE"));
+        assertEquals(Color.RED, Color.fromString("RED"));
+        assertEquals(Color.GREEN, Color.fromString("GREEN"));
+        assertEquals(Color.YELLOW, Color.fromString("YELLOW"));
+        assertEquals(Color.WHITE, Color.fromString("WHITE"));
+    }
+
+    @Test
+    public void fromString_varyingCaps_shouldReturnCorrectInstance() {
+        assertEquals(Color.BLUE, Color.fromString("Blue"));
+        assertEquals(Color.RED, Color.fromString("rEd"));
+        assertEquals(Color.GREEN, Color.fromString("green"));
+    }
+}
