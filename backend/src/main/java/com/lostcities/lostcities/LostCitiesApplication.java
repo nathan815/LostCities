097723e22aeefcbcc5ada0a050ab040768011349
Nathan Johnson <nathancj224@gmail.com>
2020-01-04 04:25:18 -0500

    Enable Spring caching

--- backend/src/main/java/com/lostcities/lostcities/LostCitiesApplication.java
+++ backend/src/main/java/com/lostcities/lostcities/LostCitiesApplication.java
@@ -2,6 +2,7 @@ package com.lostcities.lostcities;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableCaching
 public class LostCitiesApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
