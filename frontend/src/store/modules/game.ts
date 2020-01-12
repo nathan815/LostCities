3ab99ccdc748f7b6136b12e7674cb224fdedc1ba
Nathan Johnson <nathancj224@gmail.com>
2020-01-12 03:08:14 -0500

    CardView bg colors, border, numbers/handshake symbol complete.
    * Change Card from interface to a class to allow easy creation of Card objects.
    * CardView now looks like an actual "card". Color changes based on Card passed into it.
    * Shows value at top, or handshake icon if it is a wager card.

--- frontend/src/store/modules/game.ts
+++ frontend/src/store/modules/game.ts
@@ -6,10 +6,24 @@ export enum Color {
     Red = 'red',
 }
 
-export interface Card {
-    value: number;
-    color: Color;
-    instance: number;
+export class Card {
+    value: number = 0;
+    color: Color = Color.White;
+    instance?: number;
+
+    constructor(value: number, color: Color, instance?: number) {
+        this.value = value;
+        this.color = color;
+        this.instance = instance;
+    }
+
+    get isWager() {
+        return this.value === 0;
+    }
+
+    public toString() {
+        return this.color + ' ' + this.value;
+    }
 }
 
 export type Discard = {
