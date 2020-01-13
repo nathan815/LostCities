a8b66a0be878243e831a205b991a299345527631
Nathan Johnson <nathancj224@gmail.com>
2020-01-12 19:39:41 -0500

    Allow Hand component to be fixed (always visible).

--- frontend/src/views/game/Hand.vue
+++ frontend/src/views/game/Hand.vue
@@ -10,23 +10,38 @@ import CardView from '@/views/game/CardView.vue';
 export default class Hand extends Vue {
     @Prop()
     cards!: Card[];
+
+    @Prop({ default: true })
+    fixed!: boolean;
 }
 </script>
 
 <template>
-    <div class="hand">
-        <CardView v-for="card in cards" :key="card.toString()" :card="card" class="hand-card" />
-    </div>
+    <b-row>
+        <b-container class="hand" :class="{ fixed: fixed }">
+            <CardView v-for="card in cards" :key="card.toString()" :card="card" class="hand-card" />
+        </b-container>
+        <div v-if="fixed" class="spacer"></div>
+    </b-row>
 </template>
 
 <style lang="scss">
+.spacer {
+    height: 250px;
+}
 .hand {
     box-shadow: 0 -5px 5px -5px #ccc;
     padding: 20px;
-    margin: 20px auto;
     display: flex;
     flex-wrap: wrap;
     justify-content: space-around;
+    background: #f5f5f5; // TODO: add sass variable for body background
+    z-index: 2;
+    position: relative;
+    &.fixed {
+        position: fixed;
+        bottom: 0;
+    }
 }
 .hand-card {
     align-items: center;
