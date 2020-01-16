d36dcdc8912a0413a0e3370ef797befb712aa1b9
Nathan Johnson <nathancj224@gmail.com>
2020-01-15 22:48:18 -0500

    Add error handler method to GameWebsocketController

--- backend/src/main/java/com/lostcities/lostcities/web/GameWebsocketController.java
+++ backend/src/main/java/com/lostcities/lostcities/web/GameWebsocketController.java
@@ -5,31 +5,23 @@ import com.lostcities.lostcities.application.dto.MoveDto;
 import com.lostcities.lostcities.application.service.GameService;
 import com.lostcities.lostcities.domain.game.MoveException;
 import java.util.HashMap;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.annotation.SubscribeMapping;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class GameWebsocketController {
-    Logger logger = LoggerFactory.getLogger(GameWebsocketController.class);
-
-    GameService gameService;
+    private static Logger logger = LoggerFactory.getLogger(GameWebsocketController.class);
+    private GameService gameService;
 
     public GameWebsocketController(GameService gameService) {
         this.gameService = gameService;
-    }
-
-    @MessageMapping("/upper")
-    @SendTo("/topic/test")
-    public HashMap<String, String> test(HashMap<String, String> data) {
-        var resp = new HashMap<String, String>();
-        resp.put("upper", data.get("msg").toUpperCase());
-        logger.info("Test: " + resp);
-        return resp;
     }
 
     /**
@@ -46,5 +38,23 @@ public class GameWebsocketController {
     @SendTo("/topic/game/{id}")
     public GameDto gameCommand(@DestinationVariable long id, MoveDto move) throws MoveException {
         return gameService.makeMove(id, null, move);
+    }
+
+    @MessageExceptionHandler
+    @SendToUser(value = "/queue/game/errors", broadcast = false)
+    public Map<String, String> handleException(Throwable exception) {
+        logger.error("Error in game WS controller", exception);
+        var response = new HashMap<String, String>();
+        response.put("error", exception.getMessage());
+        return response;
+    }
+
+    private long parseGameId(String id) {
+        try {
+            return Long.parseLong(id);
+        } catch (NumberFormatException e) {
+            logger.error("Non-number passed as game ID" + id, e);
+            throw new RuntimeException("Game ID must be a number, but a non-number was provided: " + id, e);
+        }
     }
 }
