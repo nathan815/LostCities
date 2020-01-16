063a04e517da7bb7de4a08d55ad55aa821e88a8e
Nathan Johnson <nathancj224@gmail.com>
2020-01-16 02:49:46 -0500

    Fix issue with Deck not generating #10 cards due to wrong IntStream range
    second number in IntStream.range is exclusive, so should be 2,11 rather than 2,10

--- backend/src/test/java/com/lostcities/lostcities/domain/game/GameTest.java
+++ backend/src/test/java/com/lostcities/lostcities/domain/game/GameTest.java
@@ -14,11 +14,11 @@ public class GameTest {
         Player player2 = new Player(2L, "Player2");
         Game game = Game.create(123L, 1L, Game.Status.Started, player1, player2);
 
-        assertThat(game.getPlayer1().getHand(), is(cardSetFromStrings("BLUE_3_0", "RED_3_0", "RED_2_0", "YELLOW_6_0",
-                "GREEN_7_0", "GREEN_1_1", "WHITE_2_0", "WHITE_1_0")));
+        assertThat(game.getPlayer1().getHand(), is(cardSetFromStrings("GREEN_8_0", "YELLOW_6_0", "YELLOW_5_0",
+                "WHITE_7_0", "BLUE_9_0", "BLUE_6_0", "RED_1_2", "BLUE_7_0")));
 
-        assertThat(game.getPlayer2().getHand(), is(cardSetFromStrings("WHITE_5_0", "YELLOW_8_0", "YELLOW_3_0", "RED_1_2",
-                "GREEN_2_0", "GREEN_1_0", "GREEN_9_0", "RED_1_0")));
+        assertThat(game.getPlayer2().getHand(), is(cardSetFromStrings("RED_5_0", "BLUE_8_0", "WHITE_1_0", "YELLOW_2_0",
+                "BLUE_1_1", "RED_1_1", "RED_2_0", "WHITE_9_0")));
     }
 
 }
