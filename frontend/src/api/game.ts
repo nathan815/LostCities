05e096f61f8982e8435e9c4e81105cc93c51e38e
Nathan Johnson <nathancj224@gmail.com>
2020-01-21 22:43:55 -0500

    Rename rxjs helper toJson to parseJson

--- frontend/src/api/game.ts
+++ frontend/src/api/game.ts
@@ -2,21 +2,21 @@ import { AxiosResponse } from 'axios';
 import { map } from 'rxjs/operators';
 import apiClient from './client';
 import stompClient from '@/api/websocket/stompClient';
-import { toJson } from '@/api/websocket/helpers';
+import { parseJson } from '@/api/websocket/helpers';
 import { Move } from '@/model/game/moves';
 import { GameState } from '@/model/game';
 
-export const errorObservable = () => stompClient.watch(`/user/queue/game/errors`).pipe(toJson);
+export const errorObservable = () => stompClient.watch(`/user/queue/game/errors`).pipe(parseJson);
 
 const toGame = map((state: any) => {
     return new GameState(state);
 });
 
 export const gameStateObservable = gameId =>
-    stompClient.watch(`/topic/game/${gameId}`).pipe(toJson, toGame);
+    stompClient.watch(`/topic/game/${gameId}`).pipe(parseJson, toGame);
 
 export const userGameStateObservable = gameId =>
-    stompClient.watch(`/user/topic/game/${gameId}`).pipe(toJson, toGame);
+    stompClient.watch(`/user/topic/game/${gameId}`).pipe(parseJson, toGame);
 
 const gamePath = (gameId, path) => `/app/game/${gameId}/${path}`;
 
