4226772fe75ac579bb1d58a7258a40ddcaf3b31c
Nathan Johnson <nathancj224@gmail.com>
2020-01-23 21:09:02 -0500

    Remove unused constructor in CardNotInHandException

--- backend/src/main/java/com/lostcities/lostcities/domain/game/exception/CardNotInHandException.java
+++ backend/src/main/java/com/lostcities/lostcities/domain/game/exception/CardNotInHandException.java
@@ -3,10 +3,6 @@ package com.lostcities.lostcities.domain.game.exception;
 import com.lostcities.lostcities.domain.game.card.Card;
 
 public class CardNotInHandException extends IllegalStateException {
-    public CardNotInHandException(String message) {
-        super(message);
-    }
-
     public CardNotInHandException(Card card) {
         super("Player does not have card " + card + " in hand");
     }
